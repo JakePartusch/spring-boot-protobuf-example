c72d6d2f8198a8fb99d0e9b56ec395d214868520
Jake Partusch <jakepartusch@gmail.com>
2016-04-13 23:30:46 -0500

    adding simple json controller

---
+++ src/main/java/com/jakepartusch/domain/Person.java
@@ -0,0 +1,27 @@
+package com.jakepartusch.domain;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
