65781558c399a198ab857175fc92ea871b43b02c
Jake Partusch <jakepartusch@gmail.com>
2016-04-14 00:01:46 -0500

    configuring protobuf gradle plugin

--- src/main/java/com/jakepartusch/SpringBootProtobufApplication.java
+++ src/main/java/com/jakepartusch/SpringBootProtobufApplication.java
@@ -2,6 +2,8 @@ package com.jakepartusch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 
 @SpringBootApplication
 public class SpringBootProtobufApplication {
@@ -9,4 +11,9 @@ public class SpringBootProtobufApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProtobufApplication.class, args);
 	}
+
+    @Bean
+    ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
+    }
 }
